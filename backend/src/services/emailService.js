126e7e4bb070d3c54b1b845747ca11aa89621ccf
Animesh Toshniwal <aniaura487@gmail.com>
2025-12-04 00:51:44 +0530

    Email services changes

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -9,11 +9,15 @@ const emailService = {
       return null;
     }
     return nodemailer.createTransport({
-      service: 'gmail',
+      // service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 10000,
     });
   },
 
