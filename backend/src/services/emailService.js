12d73056b885fed285b76a6bee22bacbdeb69bf3
Animesh Toshniwal <aniaura487@gmail.com>
2026-01-15 21:57:19 +0530

    Revert "Email services changes"
    This reverts commit 126e7e4bb070d3c54b1b845747ca11aa89621ccf.

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -9,15 +9,11 @@ const emailService = {
       return null;
     }
     return nodemailer.createTransport({
-      // service: 'gmail',
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000,
     });
   },
 
