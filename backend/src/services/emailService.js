8ea89ce13931b5fe86673dbc09a04b726214c16b
Animesh Toshniwal <aniaura487@gmail.com>
2026-03-28 01:49:49 +0530

    fix: IPv6 to IPv4 in nodemailer

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -16,6 +16,9 @@ const emailService = {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      }
     });
   },
 
